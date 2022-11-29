5e9d34c7520bb40f6f758f8bae37e889b143deaa
Jamal Davis <davisjamal647@gmail.com>
2022-11-29 17:06:50 +0000

    login

--- server/index.js
+++ server/index.js
@@ -11,20 +11,27 @@ const socketIO = require("socket.io")(http, {
 
 app.use(cors());
 let users = [];
+let regUsers = [];
 
 socketIO.on("connection", (socket) => {
-    console.log("user has connected");
+    console.log(`user has connected - ${socket.id}`);
     socket.on("message", msg => {
-      socket.emit("chatResponse", msg)
+      socketIO.emit("chatResponse", msg)
     })
+    console.log(socket.id)
 
     socket.on("newUser", data => {
         users.push(data)
         socketIO.emit("newUserResponse", users)
     })
 
+    socket.on("newRegUser", data => {
+        regUsers.push(data)
+        socketIO.emit("newRegUserResponse", regUsers)
+    })
+
     socket.on("disconnect", () => {
-        console.log("user disconnected");
+        console.log(`user disconnected - ${socket.id}`);
         users = users.filter((user) => socket.id !== user.socketID)
         socketIO.emit("newUserResponse", users)
         socket.disconnect()
